ad4b68654d4a0e679fee04692367511b68822213
liulibzz <liulibzz@163.com>
2023-08-30 14:39:37 +0800

    chore :  eslint prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    // 将每行的最大字符数设置为 120
+    printWidth: 120,
+
+    // 设置每个缩进级别的空格数为 4
+    tabWidth: 4,
+
+    // 在语句末尾添加分号
+    semi: true,
+
+    // 尽可能使用单引号表示字符串
+    singleQuote: true,
+
+    // 同样使用单引号表示 JSX 属性
+    jsxSingleQuote: true,
+
+    // 在数组、对象等处尽可能添加尾随逗号
+    trailingComma: 'all',
+
+    // 在括号及其内容之间添加空格
+    bracketSpacing: true,
+
+    // 避免在箭头函数中对单个参数加上括号
+    arrowParens: 'avoid',
+
+    // 使用空格而非制表符进行缩进
+    useTabs: false,
+
+    // 不需要特殊注释（例如“// @format”）来激活文件的格式化
+    requirePragma: false,
+};
