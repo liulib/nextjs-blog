a7c3270571181e37f29a82d5134ac164b6479b4b
liulibzz <liulibzz@163.com>
2023-08-30 14:11:45 +0800

    feat：route organized

--- app/(frontend)/(home)/page.tsx
+++ app/(frontend)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
     return (
         <div className='max-w-5xl mx-auto'>
+            <p>route: /</p>
             <Button>Click me</Button>
         </div>
     );
