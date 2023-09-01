c8b86818b026feef994ce229704727e7e8303113
liulibzz <liulibzz@163.com>
2023-09-01 18:17:10 +0800

    feat: finish theme change

--- components/widget/front-header/toggle.tsx
+++ components/widget/front-header/toggle.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import { useEffect } from 'react';
 
 import Light from '@/icons/light';
 import Dark from '@/icons/dark';
@@ -9,14 +8,15 @@ import Dark from '@/icons/dark';
 export default function Toggle() {
     const { theme, setTheme } = useTheme();
 
-    useEffect(() => {
-        console.log('-----------');
-        console.log(theme);
-    }, [theme]);
+    const changeTheme = () => {
+        theme === 'light' && setTheme('dark');
+        theme === 'dark' && setTheme('light');
+    };
 
     return (
-        <div className='cursor-pointer hover:bg-slate-100 rounded-sm hidden md:block'>
-            <Light></Light>
+        <div className='cursor-pointer hover:bg-slate-100 rounded-sm hidden md:block' onClick={changeTheme}>
+            {theme === 'light' && <Light></Light>}
+            {theme === 'dark' && <Dark></Dark>}
         </div>
     );
 }
