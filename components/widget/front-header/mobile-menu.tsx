b463c385f0fd06a0ee52a438dd066ac696a4fffc
liulibzz <liulibzz@163.com>
2023-09-01 17:42:05 +0800

    fix: mobile-menu add key

--- components/widget/front-header/mobile-menu.tsx
+++ components/widget/front-header/mobile-menu.tsx
@@ -12,7 +12,10 @@ const MobileMenu: FC<MobileMenuProps> = ({ open }) => {
         <ul className={cn('md:hidden', { block: open }, { hidden: !open })}>
             {menu.map(item => {
                 return (
-                    <li className='group flex items-center gap-x-6 border-b border-dashed border-black/30 bg-gray-50 p-3 text-base font-semibold leading-7 text-gray-600 transition-colors hover:bg-gray-200 dark:border-slate-400/40 dark:bg-slate-800 dark:text-slate-400 dark:hover:bg-slate-700'>
+                    <li
+                        key={item.url}
+                        className='group flex items-center gap-x-6 border-b border-dashed border-black/30 bg-gray-50 p-3 text-base font-semibold leading-7 text-gray-600 transition-colors hover:bg-gray-200 dark:border-slate-400/40 dark:bg-slate-800 dark:text-slate-400 dark:hover:bg-slate-700'
+                    >
                         <div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg border border-black/10 bg-gray-100 shadow-md shadow-black/5 transition duration-200 group-hover:bg-gray-50 dark:border-white/10 dark:bg-slate-700 dark:shadow-slate-900/80  dark:group-hover:bg-slate-700'>
                             <item.icon className='h-6 w-6 text-gray-600 dark:text-slate-400' aria-hidden='true' />
                         </div>
